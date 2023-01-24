98916de11fd66a8428ca0604e930335f526b27d0
jacobbamio <islote.runa0k@icloud.com>
2023-01-24 17:09:01 +0100

    Making main.py fast

--- pys/main.py
+++ pys/main.py
@@ -7,30 +7,14 @@ from sklearn.preprocessing import MinMaxScaler
 import folium
 from streamlit_folium import st_folium
 import os
-import joblib
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 
 st.set_page_config(page_title="Predict your house", page_icon="🏨", layout= "wide")
 
+check_prediction_pressed = False
 
-# load the data
-
-madrid = os.path.join(abs_path, "..", "resources", "datasets", "madrid.csv")
-barcelona = os.path.join(abs_path, "..", "resources", "datasets", "barcelona.csv")
-london = os.path.join(abs_path, "..", "resources", "datasets", "london.csv")
-
-d_csvs, d_names = dict(), dict()
-
-d_csvs["csvs1"] = [madrid, barcelona]
-d_csvs["csvs2"] = [london]
-
-d_names["names1"] = ["madrid","barcelona"]
-d_names["names2"] = ["london"]
-
-
-city_instance_mb = ac.airbnb(d_csvs["csvs1"], d_names["names1"], "csv")
-city_instance_london = ac.airbnb(d_csvs["csvs2"], d_names["names2"], "csv")
+# Store data in Streamlit caché
 
 @st.cache(allow_output_mutation=True)
 def load_ci_madrid_barcelona():
@@ -40,6 +24,40 @@ def load_ci_madrid_barcelona():
 def load_ci_london():
     return city_instance_london.load_model_joblib(os.path.join(abs_path, "..", "resources", "models", "model_london.gz"))
 
+@st.cache(allow_output_mutation=True)
+def load_madrid_csv():
+    return os.path.join(abs_path, "..", "resources", "datasets", "madrid.csv")
+
+@st.cache(allow_output_mutation=True)
+def load_barcelona_csv():
+    return os.path.join(abs_path, "..", "resources", "datasets", "barcelona.csv")
+
+@st.cache(allow_output_mutation=True)
+def load_london_csv():
+    return os.path.join(abs_path, "..", "resources", "datasets", "london.csv")
+
+@st.cache(allow_output_mutation=True)
+def create_instance_mb():
+    d_csvs, d_names = dict(), dict()
+    d_csvs["csvs1"] = [madrid, barcelona]
+    d_names["names1"] = ["madrid","barcelona"]
+
+    return ac.airbnb(d_csvs["csvs1"], d_names["names1"], "csv")
+
+@st.cache(allow_output_mutation=True)
+def create_instance_london():
+    d_csvs, d_names = dict(), dict()
+    d_csvs["csvs2"] = [london]
+    d_names["names2"] = ["london"]
+
+    return ac.airbnb(d_csvs["csvs2"], d_names["names2"], "csv")
+
+madrid = load_madrid_csv()
+barcelona = load_barcelona_csv()
+london = load_london_csv()
+
+city_instance_mb = create_instance_mb()
+city_instance_london = create_instance_london()
 
 model_madrid_barcelona = load_ci_madrid_barcelona()
 model_london = load_ci_london()
@@ -55,10 +73,6 @@ city_encoder_london = city_instance_london.load_model_pickle(os.path.join(abs_pa
 neighbourhood_encoder_london = city_instance_london.load_model_pickle(os.path.join(abs_path, "..", "resources", "models", "neighbourhood_encoder_london"), "pkl")
 
 
-# Setting streamlit page
-
-
-
 tab_model, tab_mapas = st.tabs(["Predictive model","Map of your neighbourhood"])
 
 
@@ -66,6 +80,63 @@ with tab_model:
 
     st.title('Predictive model')
 
+
+# Asking the user the features of their space
+
+    city = st.selectbox('Select your city', options = ["Madrid", "Barcelona", "London"])
+
+    if city == "Madrid" or city == "Barcelona":
+
+        df_city = city_instance_mb.return_initial_df()
+
+    else: 
+
+        df_city = city_instance_london.return_initial_df()
+
+    house_type = st.selectbox('Select the type of the space', options = ['Entire home/apt', 'Private room', 'Shared room'])
+
+    d_room_type = {'Entire home/apt': 0 , 'Private room': 0, 'Shared room' : 0}
+
+    d_room_type[house_type] = 1
+
+    neighbourhood = st.selectbox('Select the neighbourhood', options = df_city[df_city["city"]==city.lower()]["neighbourhood_cleansed"].unique())
+
+    host_total_listings_count = st.slider("Selectyour listings counts", 0, 100)
+
+    accommodates =st.slider("Select the number of acomodates", 0, 15)
+
+    bathrooms_text = st.slider("Select the number of bathrooms", 0, 5)
+
+    bedrooms = st.slider("Select number of bedrooms", 0, 10)
+
+    beds = st.slider("Select number of bed", 0, 20)
+
+    minimum_nights = st.slider("Select minimun nights", 0, 365)
+
+    maximum_nights = st.slider("Select maximun nights", 0, 365)
+
+    availability_365 = st.slider("Select the days it will be avaliable in a year", 0, 365)
+
+    number_of_reviews = st.slider("Select the number of reviews you have in air bnb", 0, 10000)
+
+    reviews_per_month = st.slider("Select the number of reviews per month you have in air bnb", 0, 50)
+
+    
+    st.subheader("Amenities")
+
+    col_1, col_2 = st.columns(2)
+
+    check_amenitie0 = col_1.checkbox("Long term stays allowed",help="")
+    check_amenitie1 = col_1.checkbox("Cooking basics",help="")
+    check_amenitie2 = col_1.checkbox("Dishes and silverware",help="")
+    check_amenitie3 = col_1.checkbox("Essentials",help="")
+    check_amenitie4 = col_1.checkbox("Coffee maker",help="")
+
+    check_amenitie5 = col_2.checkbox("Hair dryer",help="")
+    check_amenitie6 = col_2.checkbox("Microwave",help="")
+    check_amenitie7 = col_2.checkbox("Refrigerator",help="")
+    check_amenitie8 = col_2.checkbox("Heating",help="")
+    check_amenitie9 = col_2.checkbox("Air conditioning",help="")
 
 
     l_amenities = [check_amenitie0, check_amenitie1, check_amenitie2,check_amenitie3 , check_amenitie4,check_amenitie5 , check_amenitie6, check_amenitie7, check_amenitie8,  check_amenitie9 ]
@@ -112,63 +183,6 @@ with tab_model:
     check_prediction = st.button("Ready, give me the prediction!!",help="")
 
     if check_prediction:
-
-        # Asking the user the features of their space
-
-        city = st.selectbox('Select your city', options = ["Madrid", "Barcelona", "London"])
-
-        if city == "Madrid" or city == "Barcelona":
-
-            df_city = city_instance_mb.return_initial_df()
-
-        else: 
-
-            df_city = city_instance_london.return_initial_df()
-
-        house_type = st.selectbox('Select the type of the space', options = ['Entire home/apt', 'Private room', 'Shared room'])
-
-        d_room_type = {'Entire home/apt': 0 , 'Private room': 0, 'Shared room' : 0}
-
-        d_room_type[house_type] = 1
-
-        neighbourhood = st.selectbox('Select the neighbourhood', options = df_city[df_city["city"]==city.lower()]["neighbourhood_cleansed"].unique())
-
-        host_total_listings_count = st.slider("Selectyour listings counts", 0, 100)
-
-        accommodates =st.slider("Select the number of acomodates", 0, 15)
-
-        bathrooms_text = st.slider("Select the number of bathrooms", 0, 5)
-
-        bedrooms = st.slider("Select number of bedrooms", 0, 10)
-
-        beds = st.slider("Select number of bed", 0, 20)
-
-        minimum_nights = st.slider("Select minimun nights", 0, 365)
-
-        maximum_nights = st.slider("Select maximun nights", 0, 365)
-
-        availability_365 = st.slider("Select the days it will be avaliable in a year", 0, 365)
-
-        number_of_reviews = st.slider("Select the number of reviews you have in air bnb", 0, 10000)
-
-        reviews_per_month = st.slider("Select the number of reviews per month you have in air bnb", 0, 50)
-
-        
-        st.subheader("Amenities")
-
-        col_1, col_2 = st.columns(2)
-
-        check_amenitie0 = col_1.checkbox("Long term stays allowed",help="")
-        check_amenitie1 = col_1.checkbox("Cooking basics",help="")
-        check_amenitie2 = col_1.checkbox("Dishes and silverware",help="")
-        check_amenitie3 = col_1.checkbox("Essentials",help="")
-        check_amenitie4 = col_1.checkbox("Coffee maker",help="")
-
-        check_amenitie5 = col_2.checkbox("Hair dryer",help="")
-        check_amenitie6 = col_2.checkbox("Microwave",help="")
-        check_amenitie7 = col_2.checkbox("Refrigerator",help="")
-        check_amenitie8 = col_2.checkbox("Heating",help="")
-        check_amenitie9 = col_2.checkbox("Air conditioning",help="")
 
         # Final prediction
 
